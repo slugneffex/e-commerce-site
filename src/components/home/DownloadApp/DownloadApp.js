2977c0e36d835e6ca99b8d3054960d6a52d9626e
slugneffex <rawatmayank969@gmail.com>
2023-03-25 18:43:46 +0530

    wcc & downloadApp section completed

--- src/components/home/DownloadApp/DownloadApp.js
+++ src/components/home/DownloadApp/DownloadApp.js
@@ -1,28 +1,30 @@
 import React from 'react';
-
 import "./DownloadApp.css"
 
 const DownloadApp = () => {
-  return (
-    <div>
-        <section className="coming-soon">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6 text-left">
-                        <h2>Download The <span>App</span> Now!</h2>
-                        <h5 className='signUpText'>Use code <span className='codeText'>SINGUP10</span> and get upto <strong>Extra 10% off</strong> on your first order</h5>
-                        {/* QR */}
-                    </div>
-                    <div className="col-md-6">
-                        <div className="mockup-img">
-                            <img src="assets/img/mockup.png" alt=""/>
+    return (
+        <div>
+            <section className="coming-soon" style={{ marginTop: "1rem", backgroundColor: "#FFF4E3", }}>
+                <div className="container">
+                    <div className="row" style={{ paddingTop: "2rem" }}>
+                        <div className="col-md-6 text-left" style={{ paddingTop: "4rem" }}>
+                            <h2 className='downloadAppHeading'>Download The <span style={{ color: "#FE9E2D", }}>App</span> Now!</h2>
+                            <h6 className='signUpText' style={{ marginTop: "1.5rem" }}>Use code <span className='codeText'>SIGNUP10</span> and get upto <strong>Extra 10% off</strong> on your first order</h6>
+                            <div className="QR">
+                                <img src="./assets/img/QR/onelinkto_add7up.png" alt="QR" width="150px" height="150px" />
+                                <p style={{color: "#464646" }}>*On the min purchase of `999</p>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="mockup-img">
+                                <img src="./assets/img/downloadAppImg/CN_Mock.png" alt="mobileAppImg" height="450px" />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    </div>
-  )
+            </section>
+        </div>
+    )
 }
 
 export default DownloadApp;
