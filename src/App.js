ed9d57128e206fc92262a4b3d982ee91f4ccf70b
reeriiraa <reemagahtori100@gmail.com>
2023-03-29 15:38:51 +0530

    Major UI Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import React from "react";
-import Account from "./pages/auth/Account";
+
 import Home from "./pages/index";
 import Cart from "./pages/cart/Cart";
 import Product from "./pages/product/Product";
@@ -9,15 +9,23 @@ import Category from "./pages/category/Category";
 import VerifyOtp from "./pages/auth/VerifyOtp";
 import SignUp from "./pages/auth/SignUp";
 import SignIn from "./pages/auth/SignIn";
-import Adress from "./pages/adress/Adress";
+
+
+
+
 import Singleproduct from "./pages/product/Singleproduct";
 import BrandProduct from "./pages/category/BrandProduct";
 import MYOC from "./pages/MYOC/MYOC";
 import Emptycart from "./pages/emptyCart/Emptycart";
-import Payment from "./pages/payment/Payment"
+import Payment from "./pages/payment/Payment";
 import Loginadress from "./pages/adress/Loginadress";
 import WCC from "./pages/WCC/WCC.js";
 import Store from "./pages/Store/Store";
+
+
+
+
+import Thanks from "./pages/thankyou/Thanks";
 
 const App = () => {
   return (
@@ -30,10 +38,14 @@ const App = () => {
       <Route path="/category/:id" element={<Category />} />
       <Route path="/brand/:brand_id" element={<BrandProduct />} />
       <Route path="/VerifyOtp" element={<VerifyOtp />} />
-      <Route path="/Account" element={<Account />} />
+      
+      <Route path="/Thanks" element={<Thanks />} />
+
+      
+      
       <Route path="/Signup" element={<SignUp />} />
       <Route path="Signin" element={<SignIn />} />
-      <Route path="/adress" element={<Adress />} />
+      
       <Route path="/view-all-products" element={<MYOC />} />
       <Route path ="/EmptyCart" element={<Emptycart />} />
       <Route path="/payment" element={<Payment />} />
