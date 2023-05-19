e239530d756d99969abedc530a7853ebcd67555d
Manish Tiwari <manishtiwari@192.168.0.105>
2023-05-19 18:33:40 +0530

    loader

--- src/components/home/Loader/Loader.js
+++ src/components/home/Loader/Loader.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import "./Loader.css"
 import { useEffect } from 'react';
-import { useSelector, useDispatch } from "react-redux";
-import { fetchByoc } from "../../features/actions/byocActions";
 
 
 
 
 const Loader = () => {
-    const dispatch = useDispatch();
+    
 
     useEffect(() => {
         document.body.classList.add("custom-scroll");
@@ -17,29 +15,14 @@ const Loader = () => {
         };
     }, []);
 
-    // For logo
-
-    const { byoc } = useSelector((state) => state.byoc);
-
-    useEffect(() => {
-        dispatch(fetchByoc());
-    }, [dispatch]);
+  
 
 
     return (
         <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
 
 
-            {Array.isArray(byoc) &&
-                byoc.map((e) => (
-                    <div key={e.id}>
-                        <img
-                            src={e.logo?.original_url}
-                            alt="logo-combonation"
-                            className='loaderImg'
-                        ></img>
-                    </div>
-                ))}
+           
 
 
 
