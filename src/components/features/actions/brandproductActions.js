239a6971e8b114ceecb4ba6c43ec5a27e830c4e5
Manish Tiwari <manishtiwari@192.168.0.111>
2023-05-26 14:38:23 +0530

    share page

--- src/components/features/actions/brandproductActions.js
+++ src/components/features/actions/brandproductActions.js
@@ -36,7 +36,7 @@ export const updateBrandproduct = (brandproduct) => ({
   payload: brandproduct,
 });
 
-export const fetchBrandproduct = (brand_id,pageNumber) => {
+export const fetchBrandproduct = (brand_id, pageNumber) => {
   return async (dispatch) => {
     dispatch(fetchBrandproductRequest());
     const options = {
@@ -53,11 +53,8 @@ export const fetchBrandproduct = (brand_id,pageNumber) => {
         `${process.env.REACT_APP_BASE_URL}/brand/${brand_id}?page=${pageNumber}`,
         options
       );
-  
+
       dispatch(fetchBrandproductSuccess(response.data));
-    
-      
- 
     } catch (error) {
       dispatch(fetchBrandproductFailure(error.message));
     }
