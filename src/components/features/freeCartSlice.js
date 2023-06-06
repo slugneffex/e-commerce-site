a287244efe26a713c61dcf888786d760512ab761
Manish Tiwari <manishtiwari@192.168.0.102>
2023-06-06 18:16:09 +0530

    freecartitems algo done

--- src/components/features/freeCartSlice.js
+++ src/components/features/freeCartSlice.js
@@ -1,49 +1,57 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const freeCartSlice = createSlice({
-    name: "freebies",
-    initialState: {
-      freecartItems: [],
-      freeCount: 0,
-     
-    },
+  name: "freebies",
+  initialState: {
+    freecartItems: [],
+    freeCount: 0,
+  },
 
-reducers: {
-    freeaddCartProduct:{
-        reducer: (state, action) => {
-            let freebiescartIndex = state.freecartItems.findIndex(
-              (item) => item.id === action.payload.id
-            );
-    
-            if (freebiescartIndex >= 0) {
-              state.freecartItems[freebiescartIndex].quantity += 1;
-            } else {
-              let tempProduct = { ...action.payload, quantity: 1 };
-              state.freecartItems.push(tempProduct);
-            }
-          },
+  reducers: {
+    freeaddCartProduct: {
+      reducer: (state, action) => {
+        let freebiescartIndex = state.freecartItems.findIndex(
+          (item) => item.id === action.payload.id
+        );
+
+        if (freebiescartIndex >= 0) {
+          state.freecartItems[freebiescartIndex].quantity += 1;
+        } else {
+          let tempProduct = { ...action.payload, quantity: 1 };
+          state.freecartItems.push(tempProduct);
+        }
+      },
     },
 
     getfreeProducts: (state, action) => {
-        return {
-          ...state,
-        };
-      },
+      return {
+        ...state,
+      };
+    },
 
-      getfreeCartCount: (state, action) => {
-        let freecartCount = state.freebiescartItems.reduce((total, item) => {
-          return item.quantity + total;
-        }, 0);
-  
-        state.freeCount = freecartCount;
-      },
+    getfreeCartCount: (state, action) => {
+      let freecartCount = state.freecartItems.reduce((total, item) => {
+        return item.quantity + total;
+      }, 0);
 
-},
+      state.freeCount = freecartCount;
+    },
+
+    removefreeCartItem: (state, action) => {
+      let index = state.freecartItems.findIndex(
+        (item) => item.id === action.payload
+      );
+      if (index !== -1) {
+        state.freecartItems.splice(index, 1);
+      }
+    },
+  },
 });
 
 export const {
-    freeaddCartProduct,
-    getfreeCartCount,
-    getfreeProducts
-} = freeCartSlice.actions
-export default freeCartSlice.reducer
+  freeaddCartProduct,
+  getfreeCartCount,
+  getfreeProducts,
+  removefreeCartItem,
+} = freeCartSlice.actions;
+export default freeCartSlice.reducer;
